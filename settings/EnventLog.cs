43984cd2c4c0f4e91e5d09c719ec3d0ca93683a4
Esteban Puentes <esteban.puentes@cern.ch>
2019-10-09 13:45:51 +0200

    a

--- settings/EnventLog.cs
+++ settings/EnventLog.cs
@@ -1,14 +1,26 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using plugin;
+using System;
 using System.Collections.Generic;
 
 namespace Winagent.Settings
 {
-    public class EventLog
+    [PluginAttribute(PluginName = "Settings")]
+    public class EventLog : IOutputPlugin
+
     {
         [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
         
         [JsonProperty(PropertyName = "outputPlugins")]
         public List<OutputPlugin> OutputPlugins { get; set; }
+
+        public event EventHandler<MessageEventArgs> MessageEvent;
+
+        public void Execute(string jsonData, JObject settings)
+        {
+            
+        }
     }
 }
